bd7b376ba7c36be71119c0e2837c620350c7a28c
PedroSouza98 <pedro.henriquesouza98@gmail.com>
2021-07-26 16:27:26 -0300

    adiciona dependencias

---
+++ desafioVoo/src/main/java/com/example/desafioVoo/DesafioVooApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioVoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVooApplication.class, args);
+	}
+
+}
---
+++ desafioVoo/src/test/java/com/example/desafioVoo/DesafioVooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioVoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
